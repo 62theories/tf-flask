098522f868b3c51bb42b6caddb20745df05e49e7
62theories <s5935512005@phuket.psu.ac.th>
2021-11-17 18:11:35 +0700

    ลอง print

--- app/app.py
+++ app/app.py
@@ -71,9 +71,10 @@ def hello_world():
 @app.route("/upload", methods=['POST'])
 def upload_file():
   if request.method == 'POST':
-    # if 'file' not in request.files:
-    #     flash('No file part')
-    #     return 'file not sent'
+    print(request.files)
+    if 'file' not in request.files:
+        flash('No file part')
+        return 'file not sent'
     file = request.files['file']
     # if file.filename == '':
     #     flash('No selected file')
